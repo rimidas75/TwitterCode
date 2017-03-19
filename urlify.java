f71d0ed7fb7e611384940d19aa5e7e8fe92d7356
rimidas75 <rimidas75@gmail.com>
2017-03-18 23:34:36 -0400

    stringToURL

---
+++ urlify.java
@@ -0,0 +1,21 @@
+package ctci;
+
+public class urlify {
+	
+	public static void main(String[] args) {
+		urlify U = new urlify();
+		System.out.println(U.check("Mr John Smith    ", 13));
+		
+	}
+
+	private String check(String string, int n) {
+		for(int i = string.length() -1,j=n ;i>=0;i--,j--)
+		{
+			
+		}
+		
+		return null;
+		
+	}
+
+}
